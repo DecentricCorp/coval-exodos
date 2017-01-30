de37b56516d6a38fdf7466c6cc20e82ac6f52ab4
Shannon Code <genecyber@gmail.com>
2017-01-29 23:53:27 -0500

    Update and rename notes.txt to install-server.sh

---
+++ install-server.sh
@@ -0,0 +1,10 @@
+RUN n 0.12.18
+RUN npm install leveldown
+RUN n 0.10.36
+npm install git://github.com/DecentricCorp/bitcore-coval.git
+#npm install git://github.com/DecentricCorp/insight-api-coval.git
+#npm install soop
+#npm install dotenv
+#npm install level@1.0
+#npm install progress
+#npm install json2csv
