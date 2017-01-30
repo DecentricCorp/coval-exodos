6b7ce214e106d839bceedf6f7dae01caf60b42fa
Shannon Code <genecyber@gmail.com>
2017-01-29 23:55:24 -0500

    Update install-server.sh

--- install-server.sh
+++ install-server.sh
@@ -2,6 +2,7 @@ RUN n 0.12.18
 RUN npm install leveldown
 RUN n 0.10.36
 npm install git://github.com/DecentricCorp/bitcore-coval.git
+npm install bignum
 #npm install git://github.com/DecentricCorp/insight-api-coval.git
 #npm install soop
 #npm install dotenv
